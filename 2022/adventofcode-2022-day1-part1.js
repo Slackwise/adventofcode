fb7f5c2dbda6cb3286131a4c512d8b02e2f73ea7
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-01 08:37:42 -0600

    Scaffolding out 2022...

---
+++ 2022/adventofcode-2022-day1-part1.js
@@ -0,0 +1,5 @@
+import { getInput, ramda } from 'bootstrap.js';
+const { input, lines } = getInput(import.meta.url);
+ramda.forEach(f => this.f = f);
+
+console.log(compose);
---
+++ 2022/bootstrap.js
@@ -0,0 +1,19 @@
+import readFileSync from 'fs';
+import { basename as pathBasename } from 'path';
+import { fileURLToPath } from 'url';
+import * as ramda from 'ramda';
+
+const basename = pathBasename(fileURLToPath(import.meta.url), '.js');
+
+function getInput(fileURL) {
+  try {
+    const input = readFileSync(basename + '.txt', 'utf8');
+    const lines = input.split('\n');
+    return { input, lines ];
+  } catch (ex) {
+    console.log(`You forgot to get the input text file for "${basename}", dummy!`);
+    process.exit(1);
+  }
+}
+
+export default { getInput, ramda };
