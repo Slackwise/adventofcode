a6a7de762247909cd296f4e59061459140ba4f80
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-01 12:01:38 -0600

    Input files are now correctly one per day

--- 2022/boilerplate.js
+++ 2022/boilerplate.js
@@ -1,18 +1,18 @@
 import { readFileSync } from 'fs';
-import { basename as pathBasename } from 'path';
+import { basename } from 'path';
 import { fileURLToPath } from 'url';
 import * as ramda from 'ramda';
 
 export { ramda };
 
 export function getInput(fileURL) {
-  const basename = pathBasename(fileURLToPath(fileURL), '.js');
+  const inputName = basename(fileURLToPath(fileURL), '.js').replace(/\-part\d/, '');
   try {
-  const input = readFileSync(`input/${basename}.txt`, {encoding: 'utf8'});
+  const input = readFileSync(`input/${inputName}.txt`, {encoding: 'utf8'});
     const lines = input.split('\n');
     return { input, lines };
   } catch (ex) {
-    console.log(`You forgot to get the input text file for "${basename}", dummy!`);
+    console.log(`You forgot to get the input text file for "${inputName}", dummy!`);
     process.exit(1);
   }
 }
