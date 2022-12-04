c8c59b71917db1749951167c7ab39cf2cd4f1aed
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-04 07:17:27 -0600

    Updated template

--- 2022/adventofcode-2022-day0__TEMPLATE.js
+++ 2022/adventofcode-2022-day0__TEMPLATE.js
@@ -1,2 +1,19 @@
 import getInput from './init.js';
 const { input, lines } = getInput(import.meta.url);
+
+
+const part1Output =
+  pipe(
+  )(lines);
+
+console.log("Part 1: " + part1Output);
+
+
+// PART 2 ===================
+
+
+const part2Output =
+  pipe(
+  )(lines);
+
+console.log("Part 2: " + part2Output);
