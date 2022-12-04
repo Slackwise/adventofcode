204f0fe1011a3e1e18ac24774c2e7667872b6af4
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-04 08:34:10 -0600

    Improved 2022 day 3 parsing

--- 2022/adventofcode-2022-day3.js
+++ 2022/adventofcode-2022-day3.js
@@ -1,10 +1,8 @@
 import getInput from './init.js';
 const { input, lines } = getInput(import.meta.url);
 
-const rucksack = line => {
-  const  [ first, second ] = splitAt(line.length / 2)(line);
-  return { first, second };
-};
+const rucksack = line =>
+  map(split(''))(splitAt(line.length / 2)(line));
 
 
 const part1Output =
@@ -12,8 +10,8 @@ const part1Output =
     map(rucksack)
   )(lines);
 
-console.log("Debug:" + part1Output[1].first);
-console.log("Debug:" + part1Output[1].second);
+console.log("Debug:" + part1Output[1][0]);
+console.log("Debug:" + part1Output[1][1]);
 
 // console.log("Part 1: " + part1Output);
 
