51b38191cf5f6604c8953260b3064f323c79cb5f
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2022-12-08 16:30:08 -0600

    2022 day 4, part 1, attempt (haven't even ran it)

---
+++ 2022/adventofcode-2022-day4.js
@@ -0,0 +1,49 @@
+import { map } from 'ramda';
+import getInput from './init.js';
+const { input, lines } = getInput(import.meta.url);
+
+const assignmentRanges = sections => {
+  const assigments = split(',')(sections);
+  const [ first, second ] = map(split('-'))(assigments);
+  return {
+    first: {
+      start: first[0],
+      end:   first[1],
+    },
+    second: {
+      start: second[0],
+      end:   second[1]
+    }
+  };
+};
+
+  
+  
+const contains = (a, b) =>
+  a.start <= b.start &&
+  a.end   >= b.end;
+
+const isOverlappingAssignmentPair = (a, b) =>
+  contains(a, b) || contains(b, a);
+
+const part1Output =
+  pipe(
+    map(assignmentRanges),
+    map(isOverlappingAssignmentPair),
+    filter(eq(T)),
+    count 
+  )(lines);
+
+console.log("Part 1: " + part1Output);
+
+
+process.exit(1); // Delete after finishing part 1
+// PART 2 ===================
+
+
+const part2Output =
+  pipe(
+    sum
+  )(lines);
+
+console.log("Part 2: " + part2Output);
