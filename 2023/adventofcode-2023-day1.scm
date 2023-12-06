c2c93c1dc181d08e5dab5d7ce5ad09dbd8ea3ab7
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2023-12-05 23:07:59 -0600

    2023 d1 p1: Part 2 should work but doesn't...

--- 2023/adventofcode-2023-day1.scm
+++ 2023/adventofcode-2023-day1.scm
@@ -2,13 +2,20 @@
 
 (define input-lines (get-input-lines (current-filename)))
 
+;; Part 1:
+
 (define (extract-numbers line)
     (let ((digits (map match:substring (list-matches "[0-9]" line))))
       (string->number (string-append (car digits) (last digits)))))
 
 (define calibration-sum (fold + 0 (map extract-numbers input-lines)))
 
+(display "Part 1: ")
 (display calibration-sum)
+(display #\newline)
+
+
+;; Part 2:
 
 (define number-words '( ("one"    . 1)
                         ("two"    . 2)
@@ -26,3 +33,13 @@
             (string-replace-substring line-to-transform (car number-word) (cdr number-word)))
           line
           number-words))
+
+(define (extract-numbers line)
+    (let ((digits (map match:substring (list-matches "[0-9]" (replace-number-words line)))))
+      (string->number (string-append (car digits) (last digits)))))
+
+(define calibration-sum (fold + 0 (map extract-numbers input-lines)))
+
+(display "Part 2: ")
+(display calibration-sum)
+(display #\newline)
