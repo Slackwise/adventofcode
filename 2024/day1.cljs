be0190801b154ed606e9f775a48cad7aa0078742
Adam Flanczewski <76595+Slackwise@users.noreply.github.com>
2024-12-07 14:09:18 -0600

    2024: day1: partially complete

--- 2024/day1.cljs
+++ 2024/day1.cljs
@@ -3,8 +3,14 @@
            [promesa.core :as p]
            [nbb.core :as nbb]))
 
-(p/->>  (nbb/slurp "input/day1.txt")
-        ;; (println)
+(def num js/parseInt)
+
+(defn build-num-vecs [[lefts rights] [left right]]
+  [(conj lefts (num left)) (conj rights (num right))])
+
+(p/->>  (nbb/slurp "input/day1.txt") 
         (#(string/split % \newline))
         (map #(string/split % #"   "))
+        (reduce build-num-vecs [[] []])
+        (map sort)
         (prn))
